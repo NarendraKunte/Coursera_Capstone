1c86fb2404e238d5099680c8c361926ab998fe46
Narendra Kunte <49022117+NarendraKunte@users.noreply.github.com>
2019-03-28 15:42:45 +0530

    Add files via upload

---
+++ CapstoneProjectNotebook.py
@@ -0,0 +1,12 @@
+
+# coding: utf-8
+
+# The key purpose of this new notebook is to initially create a capstone project and gradually enhance and maintain it. Also ensure that the notebook is managed into Github account Coursera_Capstone
+
+# In[1]:
+
+
+import pandas as pd # primary data structure library
+import numpy as np  # useful for many scientific computing in Python
+print("Hello Capstone Project Course!")
+
